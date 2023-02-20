e7ad4e933d452b0c93a84a26a60e2cf91010f25a
CHOI97 <chlwjrh@gmail.com>
2023-02-20 14:18:34 +0900

    start dart

---
+++ flutter_study_start/bin/flutter_study_start.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_study_start/flutter_study_start.dart' as flutter_study_start;
+
+void main(List<String> arguments) {
+  print('Hello Dart');
+}
---
+++ flutter_study_start/test/flutter_study_start_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_study_start/flutter_study_start.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
