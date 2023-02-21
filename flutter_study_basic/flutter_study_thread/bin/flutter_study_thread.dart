387d9ed96bb6aec49f91c9b45fe3887d0eccd9d6
CHOI97 <chlwjrh@gmail.com>
2023-02-21 15:09:43 +0900

    flutter thread

--- flutter_study_basic/flutter_study_thread/bin/flutter_study_thread.dart
+++ flutter_study_basic/flutter_study_thread/bin/flutter_study_thread.dart
@@ -1,5 +1,39 @@
 import 'package:flutter_study_thread/flutter_study_thread.dart' as flutter_study_thread;
 
+/*
 void main(List<String> arguments) {
-  print('Hello world: ${flutter_study_thread.calculate()}!');
+  printOne();
+  printTwo();
+  printThree();
 }
+void printOne(){
+  print('One');
+}
+void printThree(){
+  print('Three');
+}
+void printTwo() async{
+  Future.delayed(Duration(seconds: 1),(){ // <- Duration(seconds: 1) 1초만큼 지연후에 실행
+    print('Future!!');
+  });
+  print('Two');
+}*/
+
+void main(List<String> arguments) {
+  printOne();
+  printTwo();
+  printThree();
+}
+void printOne(){
+  print('One');
+}
+void printThree(){
+  print('Three');
+}
+void printTwo() async{
+  await Future.delayed(Duration(seconds: 1),(){ // <- Duration(seconds: 1) 1초만큼 지연후에 실행
+    print('Future!!');
+  });
+  print('Two'); // await 으로 인해 코드의 실행이 멈추고, main 다른 함수가 다 진행된 후에 await 구문부터 실행된다.
+}
+
